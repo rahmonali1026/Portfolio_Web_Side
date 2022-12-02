ed0011bc718e73c85ddcc6cbe8cf884a84bb8a51
rahmonali1026 <73836760+rahmonali1026@users.noreply.github.com>
2022-12-02 17:48:13 +0500

    First Commit
    First Commit

---
+++ js/script.js
@@ -0,0 +1,147 @@
+var el_1 = document.getElementById('names');
+let el_2 = document.getElementById('posts');
+let el_3 = document.getElementById('home');
+let el_4 = document.getElementById('about');
+let el_5 = document.getElementById('education');
+let el_6 = document.getElementById('portfolio');
+let el_7 = document.getElementById('contact');
+let el_8 = document.getElementById('hi');
+let el_9 = document.getElementById('i');
+let el_10 = document.getElementById('abo');
+let el_11 = document.getElementById('abous');
+let el_12 = document.getElementById('abouts');
+let el_13 = document.getElementById('namess');
+let el_14 = document.getElementById('ages');
+let el_15 = document.getElementById('qual');
+let el_16 = document.getElementById('postss');
+let el_17 = document.getElementById('nation');
+let el_18 = document.getElementById('educations');
+let el_19 = document.getElementById('download');
+let el_20 = document.getElementById('exp');
+let el_21 = document.getElementById('compro');
+let el_22 = document.getElementById('port');
+let el_23 = document.getElementById('cont');
+let el_24 = document.getElementById('contacts');
+let el_25 = document.getElementById('send');
+let el_26 = document.getElementById('place');
+function changeSingleTwo (element_1, element_2, element_3, element_4, element_5, element_6, element_7, element_8, element_9, element_10, element_11, element_12, element_13, element_14, element_15, element_16, element_17, element_18, element_18, element_19, element_20, element_21, element_22, element_23, element_24, element_25, element_26) {
+  element_1.innerHTML = 'Абдурахмон Меликов';
+  element_2.innerHTML = 'Full-Stack Разработчик';
+  element_3.innerHTML = 'Главная';
+  element_4.innerHTML = 'О нас';
+  element_5.innerHTML = 'Образование';
+  element_6.innerHTML = 'Портфолио';
+  element_7.innerHTML = 'Контакт';
+  element_8.innerHTML = 'Привет';
+  element_9.innerHTML = '<h1>Я <span style="color:#f9ca24;">Абдурахмон Меликов</span></h1>';
+  element_10.innerHTML = 'Студент третьего курса Таджикского технического университета имени М.С. Осими в области информационной безопасности и защиты данных';
+  element_11.innerHTML = 'Обо мне';
+  element_12.innerHTML ='<span style="color:#f9ca24;">Обо</span>'+'<span style="color:white;">мне</span>';
+  element_13.innerHTML = '<span style="color:white;">Имя:</span>'+'<span style="color:#f9ca24;">Абдурахмон Меликов</span>';
+  element_14.innerHTML = '<span style="color:white;">Возраст: </span>'+'<span style="color:#f9ca24;">20</span>';
+  element_15.innerHTML = '<span style="color:white;">Квалификация:</span>'+'<span style="color:#f9ca24;">Информационная безопасность в информационной системе</span>';
+  element_16.innerHTML = '<span style="color:white;">Пост:</span>'+'<span style="color:#f9ca24;">Full-Stack Разработчик</span>';
+  element_17.innerHTML = '<span style="color:white;">Язык:</span>'+'<span style="color:#f9ca24;">Таджикский</span>';
+  element_18.innerHTML = '<span style="color:white;">Мое </span>'+'<span style="color:#f9ca24;">Образование</span>';
+  element_19.innerHTML = '<a href="#"><button id="download" class="btn">Скачать резюме<i class="fas fa-download"></i> </button></a>';
+  element_20.innerHTML = 'Годы опыта';
+  element_21.innerHTML = 'Мои завершенные проекты';
+  element_22.innerHTML = '<span style="color:white;">Мое </span>'+'<span style="color:#f9ca24;">Портфолио</span>';
+  element_23.innerHTML = '<span style="color:white;">Свяжитесь </span>'+'<span style="color:#f9ca24;">со мной</span>';
+  element_24.innerHTML = 'Контактная Информация';
+  element_25.innerHTML = 'Отправлять';
+  element_26.innerHTML = 'Душанбе Таджикистан Район Сино Улица Нусратулло Махсум дом 95/4"А"';
+  $('#element1_id').attr('placeholder','Имя');
+  $('#element2_id').attr('placeholder','Эл. адрес');
+  $('#some_input_id').attr('placeholder','Cообщение');
+}
+// ---0000000000====0000====
+function changeSingle(element_1, element_2, element_3, element_4, element_5, element_6, element_7, element_8, element_9, element_10, element_11, element_12, element_13, element_14, element_15, element_16, element_17, element_18, element_18, element_19, element_20, element_21, element_22, element_23, element_24, element_25, element_26) {
+  element_1.innerHTML = 'Аблурахмон Меликов';
+  element_2.innerHTML = 'Барномасози Умуми';
+  element_3.innerHTML = 'Асоси';
+  element_4.innerHTML = 'Дар бораи ман';
+  element_5.innerHTML = 'Тахсилот';
+  element_6.innerHTML = 'Портфол';
+  element_7.innerHTML = 'Контакт';
+  element_8.innerHTML = 'Салом';
+  element_9.innerHTML = '<h1>Ман <span style="color:#f9ca24;">Абдурахмон Меликов</span></h1>';
+  element_10.innerHTML = 'Донишҷӯи курси сеюми Донишгоҳи техникии Тоҷикистон ба номи М.C. Осимӣ дар соҳаи амнияти иттилоотӣ ва ҳифзи маълумот';
+  element_11.innerHTML = 'Дар бораи ман';
+  element_12.innerHTML ='<span style="color:#f9ca24;">Дар </span>'+'<span style="color:white;">Бораи ман</span>';
+  element_13.innerHTML = '<span style="color:white;">Ном:</span>'+'<span style="color:#f9ca24;">Абдурахмон Меликов</span>';
+  element_14.innerHTML = '<span style="color:white;">Сину сол:</span>'+'<span style="color:#f9ca24;">20</span>';
+  element_15.innerHTML = '<span style="color:white;">Тахасус:</span>'+'<span style="color:#f9ca24;">Амнияти иттилоот дар системаи иттилоотӣ</span>';
+  element_16.innerHTML = '<span style="color:white;">Пост:</span>'+'<span style="color:#f9ca24;">Барномасози умумии веб</span>';
+  element_17.innerHTML = '<span style="color:white;">Миллат:</span>'+'<span style="color:#f9ca24;">Точик</span>';
+  element_18.innerHTML = '<span style="color:white;">Омузиши </span>'+'<span style="color:#f9ca24;">Ман</span>';
+  element_19.innerHTML = '<a href="#"><button id="download" class="btn">CV-ро зеркашӣ кунед <i class="fas fa-download"></i> </button></a>';
+  element_20.innerHTML = 'Тачрибаи кори';
+  element_21.innerHTML = 'Лоиҳахои анҷом додаам';
+  element_22.innerHTML = '<span style="color:white;">Портфели </span>'+'<span style="color:#f9ca24;">Ман</span>';
+  element_23.innerHTML = '<span style="color:white;">Бо ман </span>'+'<span style="color:#f9ca24;">Тамос гиред</span>';
+  element_24.innerHTML = 'Маълумот барои тамос';
+  element_25.innerHTML = 'Фиристодан';
+  element_26.innerHTML = 'Ш. Душанбе Кучаи Нусратулло Махсум хонаи 94/5 "a"';
+  $('#element1_id').attr('placeholder','Ном');
+  $('#element2_id').attr('placeholder','Почтаи электрони');
+  $('#some_input_id').attr('placeholder','Паём');
+}
+
+// ------========-------==========------
+$(document).ready(function(){ 
+
+  $('#menu').click(function(){
+    $(this).toggleClass('fa-times');
+    $('header').toggleClass('toggle');
+  });
+
+  $(window).on('scroll load',function(){
+
+    $('#menu').removeClass('fa-times');
+    $('header').removeClass('toggle');
+
+    if($(window).scrollTop() > 0){
+      $('.top').show();
+    }else{
+      $('.top').hide();
+    }
+
+  });
+  
+  //E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+  
+
+  // smooth scrolling 
+
+  $('a[href*="#"]').on('click',function(e){
+
+    e.preventDefault();
+
+    $('html, body').animate({
+
+      scrollTop : $($(this).attr('href')).offset().top,
+
+    },
+      500, 
+      'linear'
+    );
+
+  });
+
+});
